2a24e424bf042e73d2ca2618a98457d89c42927c
Alejandro Rivera <alexrivera78@gmail.com>
2022-04-19 19:09:44 -0500

    Updated <ul> in html & created app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+function userName() {
+    let userName = prompt("Create a username?");
+    let cats = prompt("Welcome, " + userName + "! Answer for a chance to win a prize. - What is a group of cats called, CLOWDER or KINDLE?");
+    if (cats === "CLOWDER") {
+    document.write("Congratulations, " + userName + "!! You just won a free membership to the site for one year!!"); 
+    } else
+    document.write("Sorry, that is not the right answer, " + userName + ". Better luck next time!");
+}
+
+
+function felines() {
+    let felines = prompt("What are your 3 favorite felines?");
+    document.write(felines);
+}
